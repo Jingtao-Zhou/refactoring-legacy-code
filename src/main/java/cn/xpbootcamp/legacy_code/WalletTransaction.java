095060e61b85efa117f4257c310287ca864f18a7
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-10 21:02:29 +0800

    rename the enum class

--- src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
+++ src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
@@ -1,6 +1,6 @@
 package cn.xpbootcamp.legacy_code;
 
-import cn.xpbootcamp.legacy_code.enums.STATUS;
+import cn.xpbootcamp.legacy_code.enums.Status;
 import cn.xpbootcamp.legacy_code.service.WalletService;
 import cn.xpbootcamp.legacy_code.service.WalletServiceImpl;
 import cn.xpbootcamp.legacy_code.utils.IdGenerator;
@@ -16,7 +16,7 @@ public class WalletTransaction {
     private String orderId;
     private Long createdTimestamp;
     private Double amount;
-    private STATUS status;
+    private Status status;
     private String walletTransactionId;
 
 
@@ -33,7 +33,7 @@ public class WalletTransaction {
         this.sellerId = sellerId;
         this.productId = productId;
         this.orderId = orderId;
-        this.status = STATUS.TO_BE_EXECUTED;
+        this.status = Status.TO_BE_EXECUTED;
         this.createdTimestamp = System.currentTimeMillis();
     }
 
@@ -41,7 +41,7 @@ public class WalletTransaction {
         if (buyerId == null || (sellerId == null || amount < 0.0)) {
             throw new InvalidTransactionException("This is an invalid transaction");
         }
-        if (status == STATUS.EXECUTED) return true;
+        if (status == Status.EXECUTED) return true;
         boolean isLocked = false;
         try {
             isLocked = RedisDistributedLock.getSingletonInstance().lock(id);
@@ -50,21 +50,21 @@ public class WalletTransaction {
             if (!isLocked) {
                 return false;
             }
-            if (status == STATUS.EXECUTED) return true; // double check
+            if (status == Status.EXECUTED) return true; // double check
             long executionInvokedTimestamp = System.currentTimeMillis();
             // 交易超过20天
             if (executionInvokedTimestamp - createdTimestamp > 1728000000) {
-                this.status = STATUS.EXPIRED;
+                this.status = Status.EXPIRED;
                 return false;
             }
             WalletService walletService = new WalletServiceImpl();
             String walletTransactionId = walletService.moveMoney(id, buyerId, sellerId, amount);
             if (walletTransactionId != null) {
                 this.walletTransactionId = walletTransactionId;
-                this.status = STATUS.EXECUTED;
+                this.status = Status.EXECUTED;
                 return true;
             } else {
-                this.status = STATUS.FAILED;
+                this.status = Status.FAILED;
                 return false;
             }
         } finally {
