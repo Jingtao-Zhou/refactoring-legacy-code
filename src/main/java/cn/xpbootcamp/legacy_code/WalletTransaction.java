678ac08cbfbcf4c8f4c48b6c6cee9482601161db
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-10 23:03:53 +0800

    delete unnecessary id

--- src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
+++ src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
@@ -12,15 +12,12 @@ public class WalletTransaction {
     private String id;
     private Long buyerId;
     private Long sellerId;
-    private Long productId;
-    private String orderId;
     private Long createdTimestamp;
     private Double amount;
     private Status status;
-    private String walletTransactionId;
 
 
-    public WalletTransaction(String preAssignedId, Long buyerId, Long sellerId, Long productId, String orderId) {
+    public WalletTransaction(String preAssignedId, Long buyerId, Long sellerId) {
         if (preAssignedId != null && !preAssignedId.isEmpty()) {
             this.id = preAssignedId;
         } else {
@@ -31,8 +28,6 @@ public class WalletTransaction {
         }
         this.buyerId = buyerId;
         this.sellerId = sellerId;
-        this.productId = productId;
-        this.orderId = orderId;
         this.status = Status.TO_BE_EXECUTED;
         this.createdTimestamp = System.currentTimeMillis();
     }
@@ -59,7 +54,6 @@ public class WalletTransaction {
             WalletService walletService = new WalletServiceImpl();
             String walletTransactionId = walletService.moveMoney(id, buyerId, sellerId, amount);
             if (walletTransactionId != null) {
-                this.walletTransactionId = walletTransactionId;
                 this.status = Status.EXECUTED;
                 return true;
             } else {
