9c5225d33e6d19173aa0056d85cb0ef4f4a3523f
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-12 18:55:16 +0800

    rename the wallet transaction test to wallet service test

--- src/test/java/cn/xpbootcamp/legacy_code/WalletServiceTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/WalletServiceTest.java
@@ -10,15 +10,17 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class WalletTransactionTest {
+class WalletServiceTest {
     @Test
     void should_change_the_status_to_expired_when_the_create_time_is_passed_20days() throws NoSuchFieldException, IllegalAccessException, InvalidTransactionException {
+        WalletService walletService = new WalletService();
         WalletTransaction walletTransaction = new WalletTransaction("", 1L, 2L, 3);
         Field createdTimestamp = WalletTransaction.class.getDeclaredField("createdTimestamp");
         createdTimestamp.setAccessible(true);
         createdTimestamp.set(walletTransaction, LocalDateTime.now().minusDays(20).minusMinutes(1));
-        WalletService walletService = new WalletService();
+
         walletService.execute(walletTransaction);
+
         assertEquals(Status.EXPIRED, walletTransaction.getStatus());
     }
 }
