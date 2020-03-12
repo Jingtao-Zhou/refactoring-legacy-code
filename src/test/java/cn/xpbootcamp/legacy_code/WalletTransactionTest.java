b89d68fc841e5bc943293539feda9e169d4eb42c
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-12 18:36:03 +0800

    delete reflect status part

--- src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
@@ -15,12 +15,10 @@ class WalletTransactionTest {
     void should_change_the_status_to_expired_when_the_create_time_is_passed_20days() throws NoSuchFieldException, IllegalAccessException, InvalidTransactionException {
         WalletTransaction walletTransaction = new WalletTransaction("", 1L, 2L, 3);
         Field createdTimestamp = WalletTransaction.class.getDeclaredField("createdTimestamp");
-        Field status = WalletTransaction.class.getDeclaredField("status");
         createdTimestamp.setAccessible(true);
-        status.setAccessible(true);
         createdTimestamp.set(walletTransaction, LocalDateTime.now().minusDays(20).minusMinutes(1));
         WalletService walletService = new WalletService();
         walletService.execute(walletTransaction);
-        assertEquals(Status.EXPIRED, status.get(walletTransaction));
+        assertEquals(Status.EXPIRED, walletTransaction.getStatus());
     }
 }
